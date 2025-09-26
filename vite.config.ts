b902cbb41d3fe483c01fb663a201cd65e258d73b
Steven Bachimont <stevenbachimont@example.com>
2025-09-26 10:58:27 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,8 @@ export default defineConfig(({ mode }) => {
 		preview: {
 			host: '0.0.0.0',
 			port: 3000,
-			strictPort: true
+			strictPort: true,
+			allowedHosts: ['stevenbachimont.com', 'www.stevenbachimont.com', 'localhost']
 		},
 		test: {
 			workspace: [
